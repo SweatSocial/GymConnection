bbf3dbfd64d58e846e5422507db955b5e08d08a2
RodrigoRG24 <rodrigo.rym.24@gmail.com>
2023-05-04 02:23:49 -0500

    feat: Apply my-theme.scss and modules
    Se agrego el my-theme.scss para utilizar nuestra paleta de colores y se importo los modulos en el app.module.ts

--- GC/src/app/app.module.ts
+++ GC/src/app/app.module.ts
@@ -4,6 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
+import { MatCardModule}from'@angular/material/card';
+import { MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -12,7 +22,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
